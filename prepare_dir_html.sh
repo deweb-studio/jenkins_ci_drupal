ebd75bc8550152ecca6b931ca6a05be7c70e0e09
hamrant <karpilenko@3204205.no-reply.drupal.org>
2016-10-06 10:42:40 +0300

    Prepare directory script for projects without database

---
+++ prepare_dir_html.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+set -e
+
+# The directory this script is in.
+REAL_PATH=`readlink -f "${BASH_SOURCE[0]}"`
+SCRIPT_DIR=`dirname "$REAL_PATH"`
+
+# Parse the arguments.
+GHPRID=$1
+WEBROOT=$2
+PROJECT_NAME=$3
+
+# This is the directory of the checked out pull request, from Jenkins.
+ORIGINAL_DIR="${WEBROOT}/${PROJECT_NAME}/new_pull_request"
+# The directory where the checked out pull request will reside.
+ACTUAL_DIR="${WEBROOT}/${PROJECT_NAME}/build-${GHPRID}"
+# The directory where the docroot will be symlinked to.
+DOCROOT=$WEBROOT/$PROJECT_NAME/$GHPRID
+# The command will attempt to merge master with the pull request.
+BRANCH="$PROJECT_NAME-pull-request-$GHPRID"
+
+# create folder for build if it doesn't exist
+mkdir -p $ACTUAL_DIR
+# Remove build git directory if it already exists.
+rm -rf $ACTUAL_DIR/.git
+
+# TODO: Change docroot with variable
+rsync -a --delete $WORKSPACE/ $ACTUAL_DIR
+
+rm -rf $WORKSPACE/*
+rm -rf $WORKSPACE/.[!.]*
+
+echo "All site files are now at ."
